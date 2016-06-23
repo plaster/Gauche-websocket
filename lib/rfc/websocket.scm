56e97aaf50f4482672d389444401f364896d8705
Haruka Kai <plalal@gmail.com>
2016-06-24 00:03:39 +0900

    opcode

--- lib/rfc/websocket.scm
+++ lib/rfc/websocket.scm
@@ -2,12 +2,34 @@
   (use gauche.uvector)
   )
 
-(define opcode-continue       #x0)
-(define opcode-text           #x1)
-(define opcode-binary         #x2)
-(define opcode-connection     #x8)
-(define opcode-ping           #x9)
-(define opcode-pong           #xA)
+(define opcode-continue      #x0 )
+(define opcode-text          #x1 )
+(define opcode-binary        #x2 )
+(define opcode-connection    #x8 )
+(define opcode-ping          #x9 )
+(define opcode-pong          #xA )
+
+(define (symbol<-opcode opcode)
+  (case opcode
+    (( #x0 ) 'continue           )
+    (( #x1 ) 'text               )
+    (( #x2 ) 'binary             )
+    (( #x8 ) 'connection         )
+    (( #x9 ) 'ping               )
+    (( #xA ) 'pong               )
+    (else #f )
+    ))
+
+(define (opcode<-symbol s)
+  (case s
+    (( continue   )          #x0 )
+    (( text       )          #x1 )
+    (( binary     )          #x2 )
+    (( connection )          #x8 )
+    (( ping       )          #x9 )
+    (( pong       )          #xA )
+    (else (errorf "unknown symbol: ~s" s) )
+    ))
 
 (define (build-frame
 	  :key
