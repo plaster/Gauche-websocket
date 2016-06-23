1aa723d94bd1cd818b576892c420b4f7939d0788
Haruka Kai <plalal@gmail.com>
2016-06-23 23:55:21 +0900

    opcode

--- lib/rfc/websocket.scm
+++ lib/rfc/websocket.scm
@@ -1,1 +1,17 @@
-(define-module rfc.websocket)
+(define-module rfc.websocket
+  (use gauche.uvector)
+  )
+
+(define opcode-continue       #x0)
+(define opcode-text           #x1)
+(define opcode-binary         #x2)
+(define opcode-connection     #x8)
+(define opcode-ping           #x9)
+(define opcode-pong           #xA)
+
+(define (build-frame
+	  :key
+	  [ fin? #f ]
+	  [ opcode (error "opcode required") ]
+	  )
+  )
