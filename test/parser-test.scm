e1b2cfe910c2b8e8dac583556e505863be1a7381
Haruka Kai <plalal@gmail.com>
2016-07-26 01:43:46 +0900

    parser loopback test fails

--- test/parser-test.scm
+++ test/parser-test.scm
@@ -1,4 +1,6 @@
 (use gauche.test)
+(use gauche.uvector)
+(use gauche.vport)
 (use rfc.websocket)
 
 (test-start "rfc.websocket")
@@ -16,5 +18,70 @@
   (for-each (^s (test "symbol -> opcode -> symbol" s (^() ($ symbol<-opcode $ opcode<-symbol s)) eq?))
             opcode-symbol-list)
   )
+
+(define (choppend-input-port sz in)
+  (make <buffered-input-port>
+	:fill (^ (buf)
+		($ read-uvector! buf in 0
+		   $ + -1
+		   $ min sz
+		   $ u8vector-length buf) ) ) )
+
+(define (open-chopped-input-uvector sz uv)
+  ($ choppend-input-port sz
+     $ open-input-uvector uv
+     ) )
+
+(define (test-text . input-text-list)
+  (let* [[ output-text-list '() ]
+	 [ parse (dispatch-parsed-frame$
+		   :on-text (cut push! output-text <> )
+		   ) ]
+	 [ in
+	   ($ open-input-uvector
+	      $ u8vector-append
+	      $* map
+	      ($ build-frame
+		 :fin? #t
+		 :opcode opcode-text
+		 :payload-data $ string->u8vector $)
+	      input-text-list
+	      ) ]
+	 ]
+    (test "build-frame -> dispatch-parsed-frame$ text"
+	  input-text-list
+	  (^ ()
+	     (port-for-each values (pa$ parse in))
+	     output-text-list
+	     )
+	  equal?) ) )
+
+(test-text "hoge")
+
+(define (test-chopped-text chop-size . input-text-list)
+  (let* [[ output-text-list '() ]
+	 [ parse (dispatch-parsed-frame$
+		   :on-text (cut push! output-text <> )
+		   ) ]
+	 [ in
+	   ($ open-chopped-input-uvector chop-size
+	      $ u8vector-append
+	      $* map
+	      ($ build-frame
+		 :fin? #t
+		 :opcode opcode-text
+		 :payload-data $ string->u8vector $)
+	      input-text-list
+	      ) ]
+	 ]
+    (test "build-frame -> dispatch-parsed-frame$ text"
+	  input-text-list
+	  (^ ()
+	     (port-for-each values (pa$ parse in))
+	     output-text-list
+	     )
+	  equal?) ) )
+
+(test-chopped-text 100 "hoge")
 
 (test-end :exit-on-failure 1)
